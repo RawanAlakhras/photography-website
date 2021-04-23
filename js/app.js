a8d0e4b7d7070b88fe44ccbf69c8e0b71b85e937
Rawan.alakhras <ralakhras36@gmail.com>
2021-04-23 18:15:25 +0300

    complete index page

--- js/app.js
+++ js/app.js
@@ -1,1 +1,32 @@
 "use strict";
+$( "li" ).hover(
+    function() {
+        $(this).find("span").stop().animate({
+        width:"100%",
+        opacity:"1",
+      }, 400, function () {
+      })
+    }, function() {
+        $(this).find("span").stop().animate({
+        width:"0%",
+        opacity:"0",
+      }, 400, function () {
+      })
+    }
+  );
+  $(".btn-r").hover(
+    function(){
+    $(this).find("span").stop().animate({
+      width:"100%",
+      opacity:".5",
+    }, 400, function () {
+    })
+  }, function() {
+      $(this).find("span").stop().animate({
+      width:"0%",
+      opacity:"0",
+    }, 400, function () {
+    })
+  }
+
+  );
