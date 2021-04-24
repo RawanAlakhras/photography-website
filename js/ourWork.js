1b41033d5dc5c8a5a0abd2d6ab33dfce21e382bb
Rawan.alakhras <ralakhras36@gmail.com>
2021-04-24 05:28:02 +0300

    insert slider

--- js/ourWork.js
+++ js/ourWork.js
@@ -1,1 +1,52 @@
-/* https://via.placeholder.com/960x450/000 */
+
+$(document).ready(function() {
+    $('#autoWidth').lightSlider({
+        autoWidth:true,
+        loop:true,
+        slideMove:3,
+        onSliderLoad: function() {
+            $('#autoWidth').removeClass('cS-hidden');
+        } 
+    });
+  });
+  $(document).ready(function() {
+    $('#autoWidth2').lightSlider({
+        autoWidth:true,
+        loop:true,
+        slideMove:3,
+        onSliderLoad: function() {
+            $('#autoWidth').removeClass('cS-hidden');
+        } 
+    });
+  });
+  $(document).ready(function() {
+    $('#autoWidth3').lightSlider({
+        autoWidth:true,
+        loop:true,
+        slideMove:3,
+        onSliderLoad: function() {
+            $('#autoWidth').removeClass('cS-hidden');
+        } 
+    });
+  });
+  $(document).ready(function() {
+    $('#autoWidth4').lightSlider({
+        autoWidth:true,
+        loop:true,
+        slideMove:3,
+        onSliderLoad: function() {
+            $('#autoWidth').removeClass('cS-hidden');
+        } 
+    });
+  });
+  $(document).ready(function() {
+    $('#autoWidth5').lightSlider({
+        autoWidth:true,
+        loop:true,
+        slideMove:3,
+        onSliderLoad: function() {
+            $('#autoWidth').removeClass('cS-hidden');
+        } 
+    });
+  });
+  /* https://via.placeholder.com/960x450/000 */
