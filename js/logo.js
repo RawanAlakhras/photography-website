a1a81109e17aa05ab9f6582797177e6ae024ff9f
Rawan.alakhras <ralakhras36@gmail.com>
2021-04-30 02:23:51 +0300

    cange crard to drwop dawon card

---
+++ js/logo.js
@@ -0,0 +1,8 @@
+/* $('.card-body').css('display','none'); */
+$('.card h4').css('height','200px');
+$('.card h4').css('text-align','center');
+$('.card h4').css('padding','80px');
+$('.card').css('background-color','#794b57');
+$('.card').css('color','#fff');
+$('.card').css('border-radius','2rem');
+
